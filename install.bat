25b8ec13e42943e05f3313a3030cd7858ad60eaa
Augusto Melo <augustomelo92@gmail.com>
2016-08-12 17:15:43 -0300

    Bat file that create a link between the config files.

---
+++ install.bat
@@ -0,0 +1,55 @@
+@echo off
+
+call :check_Permission
+
+echo.
+echo ****************************************************
+echo *----------Windows Configuration Installer---------*
+echo * Choose one of the following options:             *
+echo *    1- Neovim-QT                                  *
+echo *    2- GVim                                       *
+echo ****************************************************
+echo.
+
+set /P option="Option: "
+
+goto case_%option%
+:case_1
+    echo Creating nvim folder under AppData
+    mkdir %localappdata%\nvim
+    cd %localappdata%\nvim
+    echo Done!
+
+    echo Creating a link to the file init.vim!
+    mklink init.vim %userprofile%\workspace\dotfiles\neovim\init.vim
+    echo Done!
+
+    echo Creating a link to the file ginit.vim!
+    mklink ginit.vim %userprofile%\workspace\dotfiles\neovim\ginit.vim
+    echo Done!
+
+    goto end_switch
+:case_2
+    cd %userprofile%
+    echo Creating a link to the file .vimrc!
+    mklink .vimrc %userprofile%\workspace\dotfiles\vim\.vimrc
+    echo Done!
+
+    echo Creating a link to the file .gvimrc!
+    mklink .gvimrc %userprofile%\workspace\dotfiles\vim\.gvimrc
+    echo Done!
+
+    goto end_switch
+:end_switch
+
+pause
+
+
+:check_Permission
+net session>nul 2>&1
+if %errorLevel% NEQ 0 (
+	echo Admin permission is required!
+	pause
+    exit
+)
+goto:eof
