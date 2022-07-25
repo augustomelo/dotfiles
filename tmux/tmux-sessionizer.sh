cfb6b6ca8c97976bbf178377997d4311e4909a5b
Augusto Melo <augustomelo92@gmail.com>
2022-07-25 10:10:05 +0100

    Fixed paths

--- tmux/tmux-sessionizer.sh
+++ tmux/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/workspace ~/util ~/util/work ~/util/personal -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find -L ~/workspace ~/util -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
