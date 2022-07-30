b6d58cc6410998f4be3f627ddfc935bd25059da4
Augusto Melo <augustomelo92@gmail.com>
2022-07-30 13:40:31 +0100

    Added a better switch config edit mapping
    In this why I changed the current working directory of the tab, this enables me to commit the files that I changed and
    access other files without disturbing my current working flow

--- neovim/lua/plugins/fuzzy_finder.lua
+++ neovim/lua/plugins/fuzzy_finder.lua
@@ -5,8 +5,7 @@ return {
     { 'nvim-lua/plenary.nvim' },
     { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
   },
-  config = function ()
-    require('telescope').setup {
+  config = function() require('telescope').setup {
       extensions = {
         fzf = {
           fuzzy = true,
@@ -52,7 +51,23 @@ return {
     vim.keymap.set('n', '<leader>ft', '<cmd>Telescope live_grep<cr>')
     vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
     vim.keymap.set('n', '<leader>fc', function()
-      require('telescope.builtin').find_files({cwd = vim.fn.expand('~/workspace/dotfiles')})
+      local dotfiles = vim.fn.expand('~/workspace/dotfiles/')
+      require('telescope.builtin').find_files({
+        cwd = dotfiles,
+        attach_mappings = function(prompt_bufnr)
+          local actions = require("telescope.actions")
+          local action_state = require("telescope.actions.state")
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+
+            vim.api.nvim_command('tabnew ' .. dotfiles .. selection[1])
+            vim.api.nvim_command('tcd' .. dotfiles)
+          end)
+
+          return true
+        end,
+      })
     end)
   end
 }
