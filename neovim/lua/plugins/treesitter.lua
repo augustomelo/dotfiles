3461e1eaf6ef0692792ccfeea309d020f5d34374
Augusto Melo <augustomelo92@gmail.com>
2022-09-02 11:28:00 +0100

    Added stick function name

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  requires = {
+    'nvim-treesitter/nvim-treesitter-context'
+  },
   run = ':TSUpdate',
   config = function()
     require'nvim-treesitter.configs'.setup {
