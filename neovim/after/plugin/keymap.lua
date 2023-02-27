541492526730136db58f5ca8cce34ee78506f07e
Augusto Melo <augustomelo92@gmail.com>
2023-02-27 15:11:05 +0000

    chore: added set wrap! keymap

--- neovim/after/plugin/keymap.lua
+++ neovim/after/plugin/keymap.lua
@@ -1,4 +1,4 @@
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 
 vim.keymap.set('c', '<C-a>', '<home>')
 vim.keymap.set('c', '<C-e>', '<end>')
@@ -16,15 +16,15 @@ vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '<leader>gn', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', '<leader>gp', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<leader>sl', vim.diagnostic.setloclist, opts)
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.formatting, opts)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts)
 
 vim.keymap.set('n', '<leader><leader>', '<c-^>', opts)
-vim.keymap.set('n', '<leader>n', '<cmd>nohlsearch<cr>', opts)
-vim.keymap.set('n', '<leader>q', '<cmd>quit<cr>', opts)
-vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', opts)
+vim.keymap.set('n', '<leader>n', '<Cmd>nohlsearch<CR>', opts)
+vim.keymap.set('n', '<leader>q', '<Cmd>quit<CR>', opts)
+vim.keymap.set('n', '<leader>w', '<Cmd>write<CR>', opts)
 
-vim.keymap.set('n', 'j', function () if vim.v.count == 0 then return 'gj' end return 'j' end, { expr = true }, opts)
-vim.keymap.set('n', 'k', function () if vim.v.count == 0 then return 'gk' end return 'k' end, { expr = true }, opts)
+vim.keymap.set('n', 'j', function() if vim.v.count == 0 then return 'gj' end return 'j' end, { expr = true }, opts)
+vim.keymap.set('n', 'k', function() if vim.v.count == 0 then return 'gk' end return 'k' end, { expr = true }, opts)
 vim.keymap.set('n', '<space>', 'za', opts)
 vim.keymap.set('n', 'Y', 'y$', opts)
 vim.keymap.set('n', '<C-h>', '<c-w>h', opts)
@@ -33,14 +33,15 @@ vim.keymap.set('n', '<C-k>', '<c-w>k', opts)
 vim.keymap.set('n', '<C-l>', '<c-w>l', opts)
 vim.keymap.set('n', '<C-Tab>', 'gt', opts)
 vim.keymap.set('n', '<C-S-tab>', 'gT', opts)
+vim.keymap.set('n', '<C-w>', '<Cmd>set wrap!', opts)
 
 vim.keymap.set({ 'n', 'i' }, '<f1>', '<nop>', opts)
 
 
 if packer_plugins["telescope.nvim"] then
-  vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-  vim.keymap.set('n', '<leader>ft', '<cmd>Telescope live_grep<cr>')
-  vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
+  vim.keymap.set('n', '<leader>ff', '<Cmd>Telescope find_files<CR>')
+  vim.keymap.set('n', '<leader>ft', '<Cmd>Telescope live_grep<CR>')
+  vim.keymap.set('n', '<leader>fb', '<Cmd>Telescope buffers<CR>')
   vim.keymap.set('n', '<leader>fc', function()
     local dotfiles = vim.fn.expand('~/workspace/dotfiles/')
     require('telescope.builtin').find_files({
@@ -62,7 +63,6 @@ if packer_plugins["telescope.nvim"] then
   end)
 
   if packer_plugins["telescope-file-browser.nvim"] then
-      vim.keymap.set('n', '<C-n>', '<cmd>Telescope file_browser<cr>')
+    vim.keymap.set('n', '<C-n>', '<Cmd>Telescope file_browser<CR>')
   end
 end
-
