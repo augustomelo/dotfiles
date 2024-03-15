b8b37ad0d9b2f806eb31721fdc63b0aafa18ecb2
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2024-03-15 08:24:35 -0300

    feat: added hurl

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -1,4 +1,14 @@
 return {
   { 'scrooloose/nerdcommenter' },
   { 'tpope/vim-fugitive' },
+  {
+    "jellydn/hurl.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    ft = "hurl",
+    opts = {
+      debug = false,
+      mode = "split",
+      formatters = {}
+    },
+  },
 }
