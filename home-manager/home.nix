91c4d287728cbca0a2649d1f3fe8bd552486ae13
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2024-04-25 20:35:03 +0100

    feat: added eza and fd for better integration with fzt

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -18,6 +18,8 @@
     docker
     docker-buildx
     docker-compose
+    eza
+    fd
     fzf-git-sh
     git
     go
@@ -47,15 +49,31 @@
 
     fzf = {
       enable = true;
+      changeDirWidgetCommand = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
+      changeDirWidgetOptions = [
+        "--preview 'eza --tree --color=always {} | head -200'"
+      ];
       colors = {
-        fg = "#e5e9f0,bg:#3b4252,hl:#81a1c1";
-        "fg+" = "#e5e9f0,bg+:#3b4252,hl+:#81a1c1";
-        info = "#eacb8a,prompt:#bf6069,pointer:#b48dac";
-        marker = "#a3be8b,spinner:#b48dac,header:#a3be8b";
-
+        "bg+" = "#3B4252";
+        bg = "#2E3440";
+        "fg+" = "#D8DEE9";
+        fg = "#D8DEE9";
+        header = "#616E88";
+        "hl+" = "#81A1C1";
+        hl = "#616E88";
+        info = "#81A1C1";
+        marker = "#81A1C1";
+        pointer = "#81A1C1";
+        prompt = "#81A1C1";
+        spinner = "#81A1C1";
       };
+      defaultCommand = "fd --hidden --strip-cwd-prefix --exclude .git";
       defaultOptions = [
         "--bind 'tab:toggle-up,btab:toggle-down,ctrl-u:preview-up,ctrl-d:preview-down'"
+      ];
+      fileWidgetCommand = "fd --type=f --hidden --strip-cwd-prefix --exclude .git";
+      fileWidgetOptions = [
+        "--preview 'bat --number --color=always --line-range :500 {}'"
       ];
     };
 
