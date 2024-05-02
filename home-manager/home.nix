d1d3504a886c5e42c5c75c9db0dd1b500f009b60
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2024-05-03 00:03:00 +0100

    fix: application laucnhers not symlinked
    Thanks to: https://github.com/nix-community/home-manager/issues/1341#issuecomment-901513436

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   username,
+  lib,
   ...
 }: {
   home = {
@@ -34,6 +35,18 @@
     vale
     zoxide
   ];
+    # https://github.com/nix-community/home-manager/issues/1341
+    # Comment used as solution: https://github.com/nix-community/home-manager/issues/1341#issuecomment-901513436
+    home.activation = {
+      aliasApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        app_folder=$(echo ~/Applications);
+        for app in $(find "$newGenPath/home-path/Applications" -type l); do
+          $DRY_RUN_CMD rm -f $app_folder/$(basename $app)
+          $DRY_RUN_CMD /usr/bin/osascript -e "tell app \"Finder\"" -e "make new alias file to POSIX file \"$(readlink $app)\" at POSIX file \"$app_folder\"" -e "set name of result to \"$(basename $app)\"" -e "end tell"
+        done
+      '';
+    };
+
 
   programs = {
     bat = {
