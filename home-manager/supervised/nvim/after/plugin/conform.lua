ef2e99a933d11f00f1103a5e7c6d0bd800ee51d9
Augusto Melo <4723788+augustomelo@users.noreply.github.com>
2024-06-13 19:45:53 +0100

    feat: remove format on save
    where as this is a cool idea, this cause more problem that it helps,
    because almost all the repos have a different formatting

--- home-manager/supervised/nvim/after/plugin/conform.lua
+++ home-manager/supervised/nvim/after/plugin/conform.lua
@@ -2,11 +2,6 @@ require("conform").setup({
   formatters_by_ft = {
     markdown = { "markdownlint" },
   },
-  format_on_save = {
-    lsp_fallback = true,
-    async = false,
-    timeout_ms = 500,
-  },
 })
 
 vim.o.formatexpr = "v:lua.require(\"conform\").formatexpr()"
